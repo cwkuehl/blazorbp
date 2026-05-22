e1472c75f6b89f710bc22308b9fda55a049a8358
wolfgang <wolfgang@cwkuehl.de>
2026-05-22 23:15:33 +0200

    Migration to net10.0.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 cd /home/wolfgang/cs/blazorbp
 dotnet publish BlazorBp
-cd /home/wolfgang/cs/blazorbp/BlazorBp/bin/Release/net9.0/publish/
+cd /home/wolfgang/cs/blazorbp/BlazorBp/bin/Release/net10.0/publish/
 dotnet BlazorBp.dll
